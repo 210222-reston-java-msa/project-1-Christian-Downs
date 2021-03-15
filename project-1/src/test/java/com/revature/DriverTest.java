19f337490f1fc029529506e8819c55053607e53a
christian-downs <christian.downs.15@gmail.com>
2021-03-15 09:33:31 -0500

    finished up

--- project-1/src/test/java/com/revature/DriverTest.java
+++ project-1/src/test/java/com/revature/DriverTest.java
@@ -3,11 +3,15 @@ package com.revature;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.sql.Timestamp;
+
 import org.junit.Before;
 import org.junit.Test;
 
 import com.revature.model.Employee;
+import com.revature.model.Reimbursement;
 import com.revature.services.EmployeeService;
+import com.revature.services.ReimbursementService;
 
 public class DriverTest {
 
@@ -28,6 +32,13 @@ public class DriverTest {
 		
 		EmployeeService eServ = new EmployeeService();
 		assertEquals(e.toString(),eServ.findEmployeeById(1).toString());
+	}
+	
+	@Test
+	public void testingReimbursementService() {
+		Reimbursement r = new Reimbursement(2,3000,Timestamp.valueOf("2021-03-09 00:00:00"),Timestamp.valueOf("2021-03-11 15:33:25"),"Renting car","", 2,1,"APPROVED","LODGING");
+		ReimbursementService rServ = new ReimbursementService();
+		assertEquals(r.toString(),r.toString());
 	}
 
 }
